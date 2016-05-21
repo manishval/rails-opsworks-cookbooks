130b01150d1e436421c458b74364e93f43234b57
manishval <manishval@hotmail.com>
2016-05-21 11:45:35 -0400

    Update metadata.rb

--- opsworks_custom_env/metadata.rb
+++ opsworks_custom_env/metadata.rb
@@ -2,4 +2,4 @@ name        "opsworks-cookbook-custom-env"
 maintainer  "Diego Durante"
 description "This recipe allow you to use environment variables inside your Rails app when you use a AWS OpsWorks stack."
 
-recipe "cookbook-leevia::custom_env", "Generate a config/application.yml file to contain all environment variables"
+recipe "opsworks-cookbook-custom-env::custom_env", "Generate a config/application.yml file to contain all environment variables"
