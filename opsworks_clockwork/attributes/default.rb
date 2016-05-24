b72da7fb3e018f79ff5a6208c9cf5f57c3d4b1f3
Manish Valechha <manishval@hotmail.com>
2016-05-24 17:51:20 -0400

    Add app dir to clockworkd

--- opsworks_clockwork/attributes/default.rb
+++ opsworks_clockwork/attributes/default.rb
@@ -1,7 +1,7 @@
 include_attribute 'deploy'
 
 default[:clockwork] = {}
-default['clockwork']['clock'] = '/lib/clock.rb'
+default['clockwork']['clock'] = './lib/clock.rb'
 default['clockwork']['log_dir'] = '/var/log/clockwork'
 default['clockwork']['pid_dir'] = '/var/run/clockwork'
 default['clockwork']['rails_env'] = 'production'
